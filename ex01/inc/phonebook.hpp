be1bc35225e0923e7a2716f50b889e8394607f27
Alisa Valiullina <avaliull@f1r5s10.codam.nl>
2026-01-19 18:32:20 +0100

    prototype phonebook, a bit silly

--- ex01/inc/phonebook.hpp
+++ ex01/inc/phonebook.hpp
@@ -6,11 +6,60 @@
 /*   By: avaliull <avaliull@student.codam.nl>              +#+                */
 /*                                                        +#+                 */
 /*   Created: 2026/01/14 17:45:18 by avaliull            #+#    #+#           */
-/*   Updated: 2026/01/14 17:59:20 by avaliull            ########   odam.nl   */
+/*   Updated: 2026/01/14 18:01:41 by avaliull            ########   odam.nl   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iostream>
 
-class PhoneBook	{
-}
+class	Contact {
+public:
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	secret;
+};
+
+class	PhoneBook {
+
+private:
+	int	last_added = -1;
+
+public:
+	Contact	contact_list[8];
+
+	void	add_contact(void) {
+		if (last_added == 7)
+			last_added = 0;
+		else
+			last_added++;
+		// todo: add check for empty strings
+		std::cout << "\nEnter first name: ";
+		std::getline(std::cin, contact_list[last_added].first_name);
+		std::cout << "\nEnter last name: ";
+		std::getline(std::cin, contact_list[last_added].last_name);
+		std::cout << "\nEnter nickname: ";
+		std::getline(std::cin, contact_list[last_added].nickname);
+		std::cout << "\nEnter phone number: ";
+		std::getline(std::cin, contact_list[last_added].phone_number);
+		std::cout << "\nEnter darkest secret: ";
+		std::getline(std::cin, contact_list[last_added].secret);
+		std::cout << "\n Contact added!";
+	};
+
+	void	DEBUG_list_contacts(void)
+	{
+		std::cout << "\nDEBUG: list of all contacts so far:\n";
+		for (int i = 0; i <= last_added; i++) {
+			std::cout << "\tContact #1:\n";
+			std::cout << "\t\tFirst name: " << contact_list[i].first_name << '\n';
+			std::cout << "\t\tLast name: " << contact_list[i].last_name << '\n';
+			std::cout << "\t\tNickname: " << contact_list[i].nickname << '\n';
+			std::cout << "\t\tPhone Number: " << contact_list[i].phone_number << '\n';
+			std::cout << "\t\tDarkest secret: " << contact_list[i].secret << '\n';
+	   }
+	}
+};
+
--- ex01/src/phonebook.cpp
+++ ex01/src/phonebook.cpp
@@ -10,10 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(
-	int argc,
-	char *argv[]
-)
+#include "phonebook.hpp"
+
+void	print_command_list(void)
 {
+	std::cout << "\nAvaliable commands:\n\n";
+	std::cout << "ADD\tadd a new contact\n";
+	std::cout << "SEARCH\tfind a contact\n";
+	std::cout << "EXIT\tdestroy all contacts and exit\n\n";
+}
+
+int	main(void)
+{
+	PhoneBook 		*phone_book = new PhoneBook;
+	bool			run_program;
+	std::string		input_buffer;
+
+	run_program = true;
+	std::cout << "\n\tWelcome to the crappy 80's phonebook app!\n\n";
+	while (run_program == true)
+	{
+		print_command_list();
+		std::getline(std::cin, input_buffer);
+		if (input_buffer == "ADD")
+			phone_book->add_contact();
+		else if (input_buffer == "SEARCH")
+			std::cout << "dummy search\n";
+		else if (input_buffer == "EXIT")
+		{
+			std::cout << "Say goodbye to your contacts!\n";
+			run_program = false;
+		}
+		else if (input_buffer == "DEBUG")
+			phone_book->DEBUG_list_contacts();
+		else
+			std::cout << "\nYou entered a wrong command, you dummy! Here's the list again:\n";
+	}
+	delete phone_book;
 	return (0);
 }

fb1162298f3ff8800533256d2ef9d863d708c7a9
Alisa Valiullina <avaliull@f1r4s5.codam.nl>
2026-01-27 20:01:01 +0100

    changed diff script to use bash process substitution isntead of manually creating tmp files. added SHEBANG!!

--- ex02/compare_logs.sh
+++ ex02/compare_logs.sh
@@ -1,7 +1,6 @@
+#!/usr/bin/env bash
+
 print_only_field2 () {
-	awk '{print $2}' $1 > $1".diff";
+	awk '{print $2}' $1;
 }
-print_only_field2 "./my.log"
-print_only_field2 "./19920104_091532.log"
-diff -y ./my.log.diff ./19920104_091532.log.diff > diff
-rm ./my.log.diff ./19920104_091532.log.diff
+diff -y <(print_only_field2 "./my.log") <(print_only_field2 "./19920104_091532.log") > diff
